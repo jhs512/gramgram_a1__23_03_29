5c3ef53d4781955cf4801da1e86f4a3164a0dfc6
jhs512 <jangka512@gmail.com>
2023-03-30 14:17:55 +0900

    getGrantedAuthorities 구현

--- src/main/java/com/ll/gramgram/boundedContext/member/entity/Member.java
+++ src/main/java/com/ll/gramgram/boundedContext/member/entity/Member.java
@@ -6,10 +6,10 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
@@ -35,6 +35,12 @@ public class Member {
 
     public List<? extends GrantedAuthority> getGrantedAuthorities() {
         List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+
+        grantedAuthorities.add(new SimpleGrantedAuthority("member"));
+
+        if ("admin".equals(username)) {
+            grantedAuthorities.add(new SimpleGrantedAuthority("admin"));
+        }
 
         return grantedAuthorities;
     }
