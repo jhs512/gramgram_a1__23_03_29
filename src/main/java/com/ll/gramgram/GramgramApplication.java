3f621396718deae23cd88577c4807ed580d4ac12
jhs512 <jangka512@gmail.com>
2023-03-31 09:47:05 +0900

    @EntityListeners 에 대한 설명

--- src/main/java/com/ll/gramgram/GramgramApplication.java
+++ src/main/java/com/ll/gramgram/GramgramApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
+@EnableJpaAuditing // @EntityListeners(AuditingEntityListener.class) 가 작동하도록 허용
 public class GramgramApplication {
 
 	public static void main(String[] args) {
