c13190c20f9ea5d0cafb31896637e2b7975a3d10
jhs512 <jangka512@gmail.com>
2023-03-31 13:41:14 +0900

    회원가입 실패하면 뒤로가기

--- src/main/java/com/ll/gramgram/base/rq/Rq.java
+++ src/main/java/com/ll/gramgram/base/rq/Rq.java
@@ -5,7 +5,6 @@ import com.ll.gramgram.boundedContext.member.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
