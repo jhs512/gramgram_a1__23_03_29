fe966c4b020451613331f3731e5485e007707a41
jhs512 <jangka512@gmail.com>
2023-04-01 15:27:19 +0900

    호감 표시 폼, 자격이 없으면 폼말고 경고 메세지

--- src/main/java/com/ll/gramgram/boundedContext/instaMember/controller/InstaMemberController.java
+++ src/main/java/com/ll/gramgram/boundedContext/instaMember/controller/InstaMemberController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
 @RequestMapping("/instaMember")
 @RequiredArgsConstructor
