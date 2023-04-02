3759f0823cac3c4cd0f24acb030bd6639fe9ac3c
jhs512 <jangka512@gmail.com>
2023-04-03 03:29:57 +0900

    InstaMemberRepository

--- src/main/java/com/ll/gramgram/boundedContext/instaMember/repository/InstaMemberRepository.java
+++ src/main/java/com/ll/gramgram/boundedContext/instaMember/repository/InstaMemberRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface InstaMemberRepository extends JpaRepository<InstaMember, Integer> {
+public interface InstaMemberRepository extends JpaRepository<InstaMember, Long> {
     Optional<InstaMember> findByUsername(String username);
 }
