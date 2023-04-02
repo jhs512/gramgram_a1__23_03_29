fddfffbd06fd9d39b0ccf24690d542a406d41bf9
jhs512 <jangka512@gmail.com>
2023-04-03 04:00:35 +0900

    LikeablePersonService

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonService.java
@@ -21,22 +21,26 @@ public class LikeablePersonService {
 
     @Transactional
     public RsData<LikeablePerson> like(Member member, String username, int attractiveTypeCode) {
-        InstaMember toInstaMember = instaMemberService.findByUsernameOrCreate(username).getData();
+        if ( member.hasConnectedInstaMember() == false ) {
+            return RsData.of("F-2", "먼저 본인의 인스타그램 아이디를 입력해야 합니다.");
+        }
 
         if (member.getInstaMember().getUsername().equals(username)) {
             return RsData.of("F-1", "본인을 호감상대로 등록할 수 없습니다.");
         }
 
+        InstaMember toInstaMember = instaMemberService.findByUsernameOrCreate(username).getData();
+
         LikeablePerson likeablePerson = LikeablePerson
                 .builder()
-                .fromInstaMember(member.getInstaMember())
-                .fromInstaMemberUsername(member.getInstaMember().getUsername())
-                .toInstaMember(toInstaMember)
-                .toInstaMemberUsername(toInstaMember.getUsername())
-                .attractiveTypeCode(attractiveTypeCode)
+                .fromInstaMember(member.getInstaMember()) // 호감을 표시하는 사람의 인스타 멤버
+                .fromInstaMemberUsername(member.getInstaMember().getUsername()) // 중요하지 않음
+                .toInstaMember(toInstaMember) // 호감을 받는 사람의 인스타 멤버
+                .toInstaMemberUsername(toInstaMember.getUsername()) // 중요하지 않음
+                .attractiveTypeCode(attractiveTypeCode) // 1=외모, 2=능력, 3=성격
                 .build();
 
-        likeablePersonRepository.save(likeablePerson);
+        likeablePersonRepository.save(likeablePerson); // 저장
 
         return RsData.of("S-1", "입력하신 인스타유저(%s)를 호감상대로 등록되었습니다.".formatted(username), likeablePerson);
     }
