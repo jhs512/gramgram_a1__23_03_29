7ccee0465a80c53480ce9d9303cbd0247b080b2f
jhs512 <jangka512@gmail.com>
2023-04-03 02:00:51 +0900

    Member.java]

--- src/main/java/com/ll/gramgram/boundedContext/member/entity/Member.java
+++ src/main/java/com/ll/gramgram/boundedContext/member/entity/Member.java
@@ -15,13 +15,13 @@ import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@Builder // Member.builder().providerTypeCode(providerTypeCode) .. 이런식으로 쓸 수 있게 해주는
+@NoArgsConstructor // @Builder 붙이면 이거 필수
+@AllArgsConstructor // @Builder 붙이면 이거 필수
 @EntityListeners(AuditingEntityListener.class) // @CreatedDate, @LastModifiedDate 작동하게 허용
-@ToString
+@ToString // 디버그를 위한
 @Entity // 아래 클래스는 member 테이블과 대응되고, 아래 클래스의 객체는 테이블의 row와 대응된다.
-@Getter
+@Getter // 아래 필드에 대해서 전부다 게터를 만든다. private Long id; => public Long getId() { ... }
 public class Member {
     @Id
     @GeneratedValue(strategy = IDENTITY)
@@ -30,14 +30,15 @@ public class Member {
     private LocalDateTime createDate;
     @LastModifiedDate // 아래 칼럼에는 값이 자동으로 들어간다.(UPDATE 할 때 마다)
     private LocalDateTime modifyDate;
-    private String providerTypeCode;
+    private String providerTypeCode; // 일반회원인지, 카카오로 가입한 회원인지, 구글로 가입한 회원인지
     @Column(unique = true)
     private String username;
     private String password;
-    @OneToOne
-    @Setter
+    @OneToOne // 1:1
+    @Setter // memberService::updateInstaMember 함수 때문에
     private InstaMember instaMember;
 
+    // 이 함수 자체는 만들어야 한다. 스프링 시큐리티 규격
     public List<? extends GrantedAuthority> getGrantedAuthorities() {
         List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
 
@@ -52,6 +53,7 @@ public class Member {
         return grantedAuthorities;
     }
 
+    // 이 회원이 본인의 인스타ID를 등록했는지 안했는지
     public boolean hasConnectedInstaMember() {
         return instaMember != null;
     }
