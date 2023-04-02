7623e66994e023e2b0d09a608a61d30edfdc225e
jhs512 <jangka512@gmail.com>
2023-04-03 03:27:50 +0900

    InstaMemberService.java

--- src/main/java/com/ll/gramgram/boundedContext/instaMember/service/InstaMemberService.java
+++ src/main/java/com/ll/gramgram/boundedContext/instaMember/service/InstaMemberService.java
@@ -23,13 +23,19 @@ public class InstaMemberService {
     }
 
     @Transactional
+    // member : 현재 로그인한 회원
+    // username : 입력한 본인 인스타 username
+    // gender : 입력한 본인의 성별
     public RsData<InstaMember> connect(Member member, String username, String gender) {
-        Optional<InstaMember> opInstaMember = findByUsername(username);
+        Optional<InstaMember> opInstaMember = findByUsername(username); // 혹시 다른 회원이 이미 입력하신 인스타 ID와 연결되었는지
 
+        // 등록이 되어있고, 성별이 U가 아니라
         if (opInstaMember.isPresent() && !opInstaMember.get().getGender().equals("U")) {
+            // 그러면 실패
             return RsData.of("F-1", "해당 인스타그램 아이디는 이미 다른 사용자와 연결되었습니다.");
         }
 
+        //
         RsData<InstaMember> instaMemberRsData = findByUsernameOrCreate(username, gender);
 
         memberService.updateInstaMember(member, instaMemberRsData.getData());
@@ -37,6 +43,7 @@ public class InstaMemberService {
         return instaMemberRsData;
     }
 
+    // InstaMember 생성
     private RsData<InstaMember> create(String username, String gender) {
         InstaMember instaMember = InstaMember
                 .builder()
@@ -63,14 +70,17 @@ public class InstaMemberService {
     public RsData<InstaMember> findByUsernameOrCreate(String username, String gender) {
         Optional<InstaMember> opInstaMember = findByUsername(username);
 
+        // 찾았다면
         if (opInstaMember.isPresent()) {
             InstaMember instaMember = opInstaMember.get();
-            instaMember.setGender(gender);
-            instaMemberRepository.save(instaMember);
+            instaMember.setGender(gender); // 성별세팅
+            instaMemberRepository.save(instaMember); // 저장
 
+            // 기존 인스타회원이랑 연결
             return RsData.of("S-2", "인스타계정이 등록되었습니다.", instaMember);
         }
 
+        // 생성
         return create(username, gender);
     }
 }
