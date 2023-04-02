0645e30899b979d92617f0ddd7b80edf14ac3192
jhs512 <jangka512@gmail.com>
2023-04-03 01:46:32 +0900

    MemberService.java 설명추가

--- src/main/java/com/ll/gramgram/boundedContext/member/service/MemberService.java
+++ src/main/java/com/ll/gramgram/boundedContext/member/service/MemberService.java
@@ -24,16 +24,20 @@ public class MemberService {
         return memberRepository.findByUsername(username);
     }
 
-    @Transactional
+    @Transactional // SELECT 이외의 쿼리에 대한 가능성이 아주 조금이라도 있으면 붙인다.
+    // 일반 회원가입(소셜 로그인을 통한 회원가입이 아님)
     public RsData<Member> join(String username, String password) {
+        // "GRAMGRAM" 해당 회원이 일반회원가입으로 인해 생성되었다는걸 나타내기 위해서
         return join("GRAMGRAM", username, password);
     }
 
+    // 내부 처리함수, 일반회원가입, 소셜로그인을 통한 회원가입(최초 로그인 시 한번만 발생)에서 이 함수를 사용함
     private RsData<Member> join(String providerTypeCode, String username, String password) {
         if (findByUsername(username).isPresent()) {
             return RsData.of("F-1", "해당 아이디(%s)는 이미 사용중입니다.".formatted(username));
         }
 
+        // 소셜 로그인을 통한 회원가입에서는 비번이 없다.
         if (StringUtils.hasText(password)) password = passwordEncoder.encode(password);
 
         Member member = Member
@@ -48,18 +52,22 @@ public class MemberService {
         return RsData.of("S-1", "회원가입이 완료되었습니다.", member);
     }
 
+    // 해당 회원에게 인스타 계정을 연결시킨다.
+    // 1:1 관계
     @Transactional
     public void updateInstaMember(Member member, InstaMember instaMember) {
         member.setInstaMember(instaMember);
-        memberRepository.save(member);
+        memberRepository.save(member); // 여기서 실제로 UPDATE 쿼리 발생
     }
 
+    // 소셜 로그인(카카오, 구글, 네이버) 로그인이 될 때 마다 실행되는 함수
     @Transactional
     public RsData<Member> whenSocialLogin(String providerTypeCode, String username) {
-        Optional<Member> opMember = findByUsername(username);
+        Optional<Member> opMember = findByUsername(username); // username 예시 : KAKAO__1312319038130912, NAVER__1230812300
 
-        if (opMember.isPresent()) return RsData.of("S-1", "로그인 되었습니다.", opMember.get());
+        if (opMember.isPresent()) return RsData.of("S-2", "로그인 되었습니다.", opMember.get());
 
-        return join(providerTypeCode, username, "");
+        // 소셜 로그인를 통한 가입시 비번은 없다.
+        return join(providerTypeCode, username, ""); // 최초 로그인 시 딱 한번 실행
     }
 }
