c0204e8b473cfa86cc27ff4025c109329abb25d5
jhs512 <jangka512@gmail.com>
2023-04-28 10:35:01 +0900

    modifyCoolTime

--- src/test/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonServiceTests.java
+++ src/test/java/com/ll/gramgram/boundedContext/likeablePerson/service/LikeablePersonServiceTests.java
@@ -6,7 +6,9 @@ import com.ll.gramgram.boundedContext.instaMember.entity.InstaMember;
 import com.ll.gramgram.boundedContext.likeablePerson.entity.LikeablePerson;
 import com.ll.gramgram.boundedContext.likeablePerson.repository.LikeablePersonRepository;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -19,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @Transactional
 @ActiveProfiles("test")
+@TestMethodOrder(MethodOrderer.MethodName.class)
 public class LikeablePersonServiceTests {
     @Autowired
     private LikeablePersonService likeablePersonService;
@@ -205,5 +208,12 @@ public class LikeablePersonServiceTests {
         LikeablePerson likeablePerson = likeablePersonRepository.findQslByFromInstaMemberIdAndToInstaMember_username(2L, "insta_user4").orElse(null);
 
         assertThat(likeablePerson.getId()).isEqualTo(1L);
+    }
+
+    @Test
+    @DisplayName("설정파일에서 호감표시에 대한 수정쿨타임 가져오기")
+    void t006() throws Exception {
+        System.out.println("likeablePersonModifyCoolTime : " + AppConfig.getLikeablePersonModifyCoolTime());
+        assertThat(AppConfig.getLikeablePersonModifyCoolTime()).isGreaterThan(0);
     }
 }
