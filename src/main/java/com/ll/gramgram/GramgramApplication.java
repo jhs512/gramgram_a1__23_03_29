9225a26db98d2678b99ecf8c62579e5a187c82b6
jhs512 <jangka512@gmail.com>
2023-05-06 02:02:11 +0900

    캐시

--- src/main/java/com/ll/gramgram/GramgramApplication.java
+++ src/main/java/com/ll/gramgram/GramgramApplication.java
@@ -2,10 +2,12 @@ package com.ll.gramgram;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableCaching
 public class GramgramApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/ll/gramgram/base/i18nConfig/CustomMessageSource.java
+++ src/main/java/com/ll/gramgram/base/i18nConfig/CustomMessageSource.java
@@ -1,5 +1,6 @@
 package com.ll.gramgram.base.i18nConfig;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 
@@ -26,8 +27,9 @@ public class CustomMessageSource extends ResourceBundleMessageSource {
         return replaceVariables(super.resolveCode(code, locale), locale);
     }
 
+    @Cacheable(cacheNames = "translation", key = "#code + ',' + #locale")
     public String replaceVariablesToString(String code, Locale locale) {
-        StringBuffer result = new StringBuffer();
+        StringBuilder result = new StringBuilder();
         Matcher matcher = VARIABLE_PATTERN.matcher(code);
 
         while (matcher.find()) {
