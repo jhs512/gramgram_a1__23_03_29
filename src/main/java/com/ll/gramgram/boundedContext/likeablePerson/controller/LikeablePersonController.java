b00eeabb43bdbcb97a877ae840f04a322d7e4d2f
jhs512 <jangka512@gmail.com>
2023-05-10 22:33:46 +0900

    성별 필터링

--- src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
+++ src/main/java/com/ll/gramgram/boundedContext/likeablePerson/controller/LikeablePersonController.java
@@ -124,7 +124,7 @@ public class LikeablePersonController {
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/toList")
-    public String showToList(Model model, String gender, @RequestParam(defaultValue = "0") int attractiveTypeCode, @RequestParam(defaultValue = "1") int sortCode) {
+    public String showToList(Model model, @RequestParam(defaultValue = "") String gender, @RequestParam(defaultValue = "0") int attractiveTypeCode, @RequestParam(defaultValue = "1") int sortCode) {
         InstaMember instaMember = rq.getMember().getInstaMember();
 
         // 인스타인증을 했는지 체크
@@ -132,8 +132,9 @@ public class LikeablePersonController {
             // 해당 인스타회원이 좋아하는 사람들 목록
             Stream<LikeablePerson> likeablePeopleStream = instaMember.getToLikeablePeople().stream();
 
-            if (gender != null) {
-                // likeablePeopleStream = likeablePeopleStream.filter();
+            if (!gender.isEmpty()) {
+                likeablePeopleStream = likeablePeopleStream
+                        .filter(likeablePerson -> likeablePerson.getFromInstaMember().getGender().equals(gender));
             }
 
             if (attractiveTypeCode != 0) {
